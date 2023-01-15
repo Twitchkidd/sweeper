5b58a536690bc279b3f702d300029d987a9e9a81
Gareth Field <field.gareth@gmail.com>
2023-01-15 11:41:28 -0500

    Saving!

--- src/views/Menu/MenuPage.js
+++ src/views/Menu/MenuPage.js
@@ -1,5 +1,19 @@
-// import styled from 'styled-components';
-
 const MenuPage = () => <p>Menu!</p>;
 
 export default MenuPage;
+
+/*
+ * So the way I picture it, there's a title and description at the top of a
+ * centered stack, and it's right aligned in the sketch, then ... it really
+ * makes sense for that to be it's own line, maybe ... so a prompt 'Name:' and
+ * an input and submit button, and then two columns below that, one with a
+ * heading of 'Top Scores:' and then for each difficulty the player and their
+ * time, and then on the other side buttons for each difficulty, and then a
+ * footer at the bottom, link to code, etc. Then when a name has been entered,
+ * where the prompt was would be one of those blank profile pic type things and
+ * the player's name, and ... somewhere, either next to the top times or next
+ * to the buttons, the player's times, or n/a, and then I realized I forgot
+ * changing player name, one step at a time here, if I can get all that onto
+ * the page we're doin' okay.
+ *
+ */
