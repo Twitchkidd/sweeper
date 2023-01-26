a79bcf9e354e6ccb82e8c0f55d17df697e15d0d0
Gareth Field <field.gareth@gmail.com>
2023-01-26 14:27:36 -0500

    Saving!

--- src/views/Game/GamePage.js
+++ src/views/Game/GamePage.js
@@ -4,10 +4,6 @@ import MenuBar from '../../components/game/MenuBar/MenuBar';
 import Game from '../../components/game/Game/Game';
 
 const GamePage = ({ difficulty, backToMenu }) => {
-	// I should take the higher level things and make them gamepage's responsibility
-	// and let the board handle the cells, and pass back up what gamebar needs
-	// okay no wait, gamebar and board are both game.
-	// Naming things is hard.
 	return (
 		<Window>
 			<TitleBar backToMenu={backToMenu} />
@@ -18,17 +14,3 @@ const GamePage = ({ difficulty, backToMenu }) => {
 };
 
 export default GamePage;
-
-// window
-// title bar
-// logo?
-// title
-// x - back nav
-// menu bar
-// game/help tabs
-// game bar
-// mine count
-// smiley face
-// timer
-// board
-// cells
